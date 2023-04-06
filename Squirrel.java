050815dde9d9580168513d366bb2e1f71ec1db12
JadynJacques <91316558+Jadout7@users.noreply.github.com>
2023-04-06 17:41:38 +0200

    🐿️ Class WITHOUT add and remove 🐿️ -Darth Jar Jar

---
+++ Squirrel.java
@@ -0,0 +1,365 @@
+public class Squirrel {
+    private String longitude;
+    private String lat;
+    private String unique_squirrel_id;
+    private String hectare;
+    private String shift;
+    private String date;
+    private String hectare_squirrel_number;
+    private String age;
+    private String primary_fur_color;
+    private String highlight_fur_color;
+    private String combination_of_primary_and_highlight_color;
+    private String color_notes;
+    private String location;
+    private String above_ground_sighter_measurement;
+    private String specific_location;
+    private String running;
+    private String chasing;
+    private String climbing;
+    private String eating;
+    private String foraging;
+    private String other_activities;
+    private String kuks;
+    private String quaas;
+    private String moans;
+    private String tail_flags;
+    private String tail_twitches;
+    private String approaches;
+    private String indifferent;
+    private String runs_from;
+    private String other_interactions;
+    private String lat_long;
+    private String zip_codes;
+    private String community_districts;
+    private String borough_boundaries;
+    private String city_council_districts;
+    private String police_precincts;
+
+    public Squirrel(String longitude, String lat, String unique_squirrel_id, String hectare, String shift, String date, String hectare_squirrel_number, String age, String primary_fur_color, String highlight_fur_color, String combination_of_primary_and_highlight_color, String color_notes, String location, String above_ground_sighter_measurement, String specific_location, String running, String chasing, String climbing, String eating, String foraging, String other_activities, String kuks, String quaas, String moans, String tail_flags, String tail_twitches, String approaches, String indifferent, String runs_from, String other_interactions, String lat_long, String zip_codes, String community_districts, String borough_boundaries, String city_council_districts, String police_precincts) {
+        this.longitude = longitude;
+        this.lat = lat;
+        this.unique_squirrel_id = unique_squirrel_id;
+        this.hectare = hectare;
+        this.shift = shift;
+        this.date = date;
+        this.hectare_squirrel_number = hectare_squirrel_number;
+        this.age = age;
+        this.primary_fur_color = primary_fur_color;
+        this.highlight_fur_color = highlight_fur_color;
+        this.combination_of_primary_and_highlight_color = combination_of_primary_and_highlight_color;
+        this.color_notes = color_notes;
+        this.location = location;
+        this.above_ground_sighter_measurement = above_ground_sighter_measurement;
+        this.specific_location = specific_location;
+        this.running = running;
+        this.chasing = chasing;
+        this.climbing = climbing;
+        this.eating = eating;
+        this.foraging = foraging;
+        this.other_activities = other_activities;
+        this.kuks = kuks;
+        this.quaas = quaas;
+        this.moans = moans;
+        this.tail_flags = tail_flags;
+        this.tail_twitches = tail_twitches;
+        this.approaches = approaches;
+        this.indifferent = indifferent;
+        this.runs_from = runs_from;
+        this.other_interactions = other_interactions;
+        this.lat_long = lat_long;
+        this.zip_codes = zip_codes;
+        this.community_districts = community_districts;
+        this.borough_boundaries = borough_boundaries;
+        this.city_council_districts = city_council_districts;
+        this.police_precincts = police_precincts;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getUnique_squirrel_id() {
+        return unique_squirrel_id;
+    }
+
+    public void setUnique_squirrel_id(String unique_squirrel_id) {
+        this.unique_squirrel_id = unique_squirrel_id;
+    }
+
+    public String getHectare() {
+        return hectare;
+    }
+
+    public void setHectare(String hectare) {
+        this.hectare = hectare;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHectare_squirrel_number() {
+        return hectare_squirrel_number;
+    }
+
+    public void setHectare_squirrel_number(String hectare_squirrel_number) {
+        this.hectare_squirrel_number = hectare_squirrel_number;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPrimary_fur_color() {
+        return primary_fur_color;
+    }
+
+    public void setPrimary_fur_color(String primary_fur_color) {
+        this.primary_fur_color = primary_fur_color;
+    }
+
+    public String getHighlight_fur_color() {
+        return highlight_fur_color;
+    }
+
+    public void setHighlight_fur_color(String highlight_fur_color) {
+        this.highlight_fur_color = highlight_fur_color;
+    }
+
+    public String getCombination_of_primary_and_highlight_color() {
+        return combination_of_primary_and_highlight_color;
+    }
+
+    public void setCombination_of_primary_and_highlight_color(String combination_of_primary_and_highlight_color) {
+        this.combination_of_primary_and_highlight_color = combination_of_primary_and_highlight_color;
+    }
+
+    public String getColor_notes() {
+        return color_notes;
+    }
+
+    public void setColor_notes(String color_notes) {
+        this.color_notes = color_notes;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAbove_ground_sighter_measurement() {
+        return above_ground_sighter_measurement;
+    }
+
+    public void setAbove_ground_sighter_measurement(String above_ground_sighter_measurement) {
+        this.above_ground_sighter_measurement = above_ground_sighter_measurement;
+    }
+
+    public String getSpecific_location() {
+        return specific_location;
+    }
+
+    public void setSpecific_location(String specific_location) {
+        this.specific_location = specific_location;
+    }
+
+    public String getRunning() {
+        return running;
+    }
+
+    public void setRunning(String running) {
+        this.running = running;
+    }
+
+    public String getChasing() {
+        return chasing;
+    }
+
+    public void setChasing(String chasing) {
+        this.chasing = chasing;
+    }
+
+    public String getClimbing() {
+        return climbing;
+    }
+
+    public void setClimbing(String climbing) {
+        this.climbing = climbing;
+    }
+
+    public String getEating() {
+        return eating;
+    }
+
+    public void setEating(String eating) {
+        this.eating = eating;
+    }
+
+    public String getForaging() {
+        return foraging;
+    }
+
+    public void setForaging(String foraging) {
+        this.foraging = foraging;
+    }
+
+    public String getOther_activities() {
+        return other_activities;
+    }
+
+    public void setOther_activities(String other_activities) {
+        this.other_activities = other_activities;
+    }
+
+    public String getKuks() {
+        return kuks;
+    }
+
+    public void setKuks(String kuks) {
+        this.kuks = kuks;
+    }
+
+    public String getQuaas() {
+        return quaas;
+    }
+
+    public void setQuaas(String quaas) {
+        this.quaas = quaas;
+    }
+
+    public String getMoans() {
+        return moans;
+    }
+
+    public void setMoans(String moans) {
+        this.moans = moans;
+    }
+
+    public String getTail_flags() {
+        return tail_flags;
+    }
+
+    public void setTail_flags(String tail_flags) {
+        this.tail_flags = tail_flags;
+    }
+
+    public String getTail_twitches() {
+        return tail_twitches;
+    }
+
+    public void setTail_twitches(String tail_twitches) {
+        this.tail_twitches = tail_twitches;
+    }
+
+    public String getApproaches() {
+        return approaches;
+    }
+
+    public void setApproaches(String approaches) {
+        this.approaches = approaches;
+    }
+
+    public String getIndifferent() {
+        return indifferent;
+    }
+
+    public void setIndifferent(String indifferent) {
+        this.indifferent = indifferent;
+    }
+
+    public String getRuns_from() {
+        return runs_from;
+    }
+
+    public void setRuns_from(String runs_from) {
+        this.runs_from = runs_from;
+    }
+
+    public String getOther_interactions() {
+        return other_interactions;
+    }
+
+    public void setOther_interactions(String other_interactions) {
+        this.other_interactions = other_interactions;
+    }
+
+    public String getLat_long() {
+        return lat_long;
+    }
+
+    public void setLat_long(String lat_long) {
+        this.lat_long = lat_long;
+    }
+
+    public String getZip_codes() {
+        return zip_codes;
+    }
+
+    public void setZip_codes(String zip_codes) {
+        this.zip_codes = zip_codes;
+    }
+
+    public String getCommunity_districts() {
+        return community_districts;
+    }
+
+    public void setCommunity_districts(String community_districts) {
+        this.community_districts = community_districts;
+    }
+
+    public String getBorough_boundaries() {
+        return borough_boundaries;
+    }
+
+    public void setBorough_boundaries(String borough_boundaries) {
+        this.borough_boundaries = borough_boundaries;
+    }
+
+    public String getCity_council_districts() {
+        return city_council_districts;
+    }
+
+    public void setCity_council_districts(String city_council_districts) {
+        this.city_council_districts = city_council_districts;
+    }
+
+    public String getPolice_precincts() {
+        return police_precincts;
+    }
+
+    public void setPolice_precincts(String police_precincts) {
+        this.police_precincts = police_precincts;
+    }
+}
