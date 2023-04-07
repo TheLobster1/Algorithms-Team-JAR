bc6ca3b663990e4e0c73faae749da031440147b7
Andreikova <112638712+Andreikova@users.noreply.github.com>
2023-04-07 14:05:25 +0200

    Insertion sort done to generics
    Recursive insertion sort

--- GenInsertionSort.java
+++ GenInsertionSort.java
@@ -1,13 +1,25 @@
 public class GenInsertionSort<T extends Comparable<? super T>> {
 
-    private int i;
+    private int j;
 
     public GenInsertionSort() {
-        i = 0;
+        j = 0;
     }
 
+    public <T extends iPoliceCollection> void genInsertionSort(T collection, int size) {
 
-    public void genInsertionSort(T[] a) {
+        if (size <= 1) {
+            return;
+        }
+        genInsertionSort(collection, size - 1);
 
+        int last = (int) collection.findIndex(size - 1);
+        j = size - 2;
+
+        while (j >= 0 && (int) collection.findIndex(j) > last) {
+            collection.swapEl(j + 1, j);
+            j--;
+        }
+        collection.swapEl(j + 1, last);
     }
 }
