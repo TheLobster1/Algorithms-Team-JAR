f3b81828d61a32373d872dc1beedaac70dea3797
JadynJacques <91316558+Jadout7@users.noreply.github.com>
2023-06-08 23:34:37 +0200

    Implemented an easier to handle Video Games sales dataset

---
+++ VgSales.java
@@ -0,0 +1,40 @@
+public class VgSales implements Comparable<VgSales>{
+    private String rank;
+    private String name;
+    private String year;
+
+    public VgSales(String rank, String name, String year) {
+        this.rank = rank;
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public int compareTo(VgSales o) {
+        return 0;
+    }
+}
