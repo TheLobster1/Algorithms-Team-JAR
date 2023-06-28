f660fa9fc9214b7b40d1925c9afec34e0ff1353b
JadynJacques <91316558+Jadout7@users.noreply.github.com>
2023-06-29 01:57:33 +0200

    Updated GUI, changed ArrayList

--- GUIPanel.java
+++ GUIPanel.java
@@ -42,7 +42,7 @@ public class GUIPanel extends JPanel {
                 String year = columns[3]; // Reasoning for this is because the dataset has N/A as some years
                 VgSales vgSales = new VgSales(rank, name, year);
                 doubly.addEnd(vgSales);
-//                array.add(vgSales);
+                array.add(vgSales);
                 singly.add(vgSales);
             }
 
@@ -164,7 +164,7 @@ public class GUIPanel extends JPanel {
                     if (singlyLinkedListToggle.isSelected()) {
                         if (bubbleSortToggle.isSelected()) {
                             stopWatch.start();
-                            singly.bubbleSort(singly.getHead(), Comparator.comparing(VgSales::getYear));
+                            singly.bubbleSort(singly.getHead(), Comparator.comparing(VgSales::getName));
                             stopWatch.stop();
                             String results = "";
                             results += "Time passed: " + (stopWatch.getElapsedTimeMillis()) + " Milliseconds \n";
@@ -173,11 +173,12 @@ public class GUIPanel extends JPanel {
                                 results += current.getData().getRank() + " | " + current.getData().getName() + " | " + current.getData().getYear() + "\n";
                                 current = current.getNext();
                             }
+                            System.out.println(results);
                             setText(results);
                         }
                         if (insertionSortToggle.isSelected()) {
                             stopWatch.start();
-                            singly.bubbleSort(singly.getHead(), Comparator.comparing(VgSales::getYear));
+                            singly.insertionSort(singly.getHead(), Comparator.comparing(VgSales::getYear));
                             stopWatch.stop();
                             String results = "";
                             results += "Time passed: " + (stopWatch.getElapsedTimeMillis()) + " Milliseconds \n";
@@ -186,6 +187,7 @@ public class GUIPanel extends JPanel {
                                 results += current.getData().getRank() + " | " + current.getData().getName() + " | " + current.getData().getYear() + "\n";
                                 current = current.getNext();
                             }
+                            System.out.println(results);
                             setText(results);
                         }
                         if (binarySearchToggle.isSelected()) {
@@ -197,11 +199,34 @@ public class GUIPanel extends JPanel {
                     }
                     if (arrayListToggle.isSelected()) {
                         if (bubbleSortToggle.isSelected()) {
-                            //todo
+                            stopWatch.start();
+                            array.bubbleSort(array, Comparator.comparing(VgSales::getName));
+                            stopWatch.stop();
 
+                            String results = "";
+                            results += "Time passed: " + stopWatch.getElapsedTimeMillis() + " Milliseconds\n";
+                            for (int i = 0; i < array.getSize(); i++) {
+                                // Type casting here needs to be changed
+                                VgSales vgsale = (VgSales) array.get(i);
+                                results += vgsale.getRank() + " | " + vgsale.getName() + " | " + vgsale.getYear() + "\n";
+                            }
+                            System.out.println(results);
+                            setText(results);
                         }
                         if (insertionSortToggle.isSelected()) {
-                            //todo
+                            stopWatch.start();
+                            array.insertionSort(array, Comparator.comparing(VgSales::getYear));
+                            stopWatch.stop();
+
+                            String results = "";
+                            results += "Time passed: " + stopWatch.getElapsedTimeMillis() + " Milliseconds\n";
+                            for (int i = 0; i < array.getSize(); i++) {
+                                // Type casting here needs to be changed
+                                VgSales vgsale = (VgSales) array.get(i);
+                                results += vgsale.getRank() + " | " + vgsale.getName() + " | " + vgsale.getYear() + "\n";
+                            }
+                            System.out.println(results);
+                            setText(results);
                         }
                         if (binarySearchToggle.isSelected()) {
                             //todo
