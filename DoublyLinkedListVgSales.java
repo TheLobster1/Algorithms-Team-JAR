1a2a4218692cd676b14502267e457c59cd053103
Andreikova <112638712+Andreikova@users.noreply.github.com>
2023-06-28 21:53:02 +0200

    An apology and hopefully working double linked list search (Binary).

--- DoublyLinkedListVgSales.java
+++ DoublyLinkedListVgSales.java
@@ -45,17 +45,18 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
             this.next = next;
         }
     }
+
     public DoublyLinkedListVgSales() {
         this.head = null;
         this.tail = null;
     }
+
     public void addFront(T data) {
         Node<T> node = new Node<>(data);
-        if(head == null) {
+        if (head == null) {
             head = node;
             tail = node;
-        }
-        else {
+        } else {
             node.setNext(head);
             head.setPrev(node);
             head = node;
@@ -64,10 +65,9 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
 
     public void addEnd(T data) {
         Node<T> node = new Node<>(data);
-        if(head == null) {
+        if (head == null) {
             head = node;
-        }
-        else {
+        } else {
             tail.setNext(node);
             node.setPrev(tail);
         }
@@ -78,16 +78,14 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
         Node<T> current = head;
         while (current != null) { //while there is a node to work with
             if (current.getData().equals(data)) {
-                if(current.getPrev() == null) {
+                if (current.getPrev() == null) {
                     head = current.getNext();
-                }
-                else {
+                } else {
                     current.getPrev().setNext(current.getNext());
                 }
-                if(current.getNext() == null) {
+                if (current.getNext() == null) {
                     tail = current.getPrev();
-                }
-                else {
+                } else {
                     current.getNext().setPrev(current.getPrev());
                 }
                 return;
@@ -103,7 +101,7 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
     }
 
     public void bubbleSort() {
-        if(head == null || head.getNext() == null) {
+        if (head == null || head.getNext() == null) {
             return; //can't be sorted
         }
         boolean hasSwapped;
@@ -114,7 +112,7 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
             hasSwapped = false;
             current = head;
             while (current.getNext() != tail) {
-                if(current.getData().compareTo(current.getNext().getData()) > 0) {
+                if (current.getData().compareTo(current.getNext().getData()) > 0) {
                     swapNodes(current, current.getNext());
                     hasSwapped = true;
                 }
@@ -133,19 +131,17 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
     }
 
     private void moveNodeToPosition(Node<T> node, Node<T> position) {
-        if(node == position) {
+        if (node == position) {
             return;
         }
-        if(node == head) {
+        if (node == head) {
             head = head.getNext();
-        }
-        else {
+        } else {
             node.getPrev().setNext(node.getNext());
         }
-        if(node == tail) {
+        if (node == tail) {
             tail = tail.getPrev();
-        }
-        else {
+        } else {
             node.getNext().setPrev(node.getPrev());
         }
         if (position == null) {
@@ -153,21 +149,20 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
             node.setPrev(tail);
             node.setNext(null);
             tail = node;
-        }
-        else {
+        } else {
             node.setPrev(position.getPrev());
             node.setNext(position);
             if (position.getPrev() != null) {
                 position.getPrev().setNext(node);
-            }
-            else {
+            } else {
                 head = node;
             }
             position.setPrev(node);
         }
     }
+
     public void insertionSort() {
-        if (head == null || head.getNext() == null){
+        if (head == null || head.getNext() == null) {
             return;
         }
         Node<T> newTail = head;
@@ -175,12 +170,11 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
 
         while (current != null) {
             Node<T> insertionPosition = getInsertionPosition(current);
-            if(insertionPosition != current) {
+            if (insertionPosition != current) {
                 Node<T> temp = current;
                 current = current.getNext();
                 moveNodeToPosition(temp, insertionPosition);
-            }
-            else {
+            } else {
                 current = current.getNext();
                 newTail = newTail.getNext();
             }
@@ -188,18 +182,17 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
     }
 
     public Node<T> linearSearch(T data) {
-        if(head == null ) {
+        if (head == null) {
             return null;
         }
-        if(data == null) {
+        if (data == null) {
             return null;
         }
         Node<T> current = head;
-        while(current != null) {
-            if(current.getData().equals(data)) {
+        while (current != null) {
+            if (current.getData().equals(data)) {
                 return current;
-            }
-            else{
+            } else {
                 current = current.getNext();
             }
         }
@@ -214,4 +207,42 @@ public class DoublyLinkedListVgSales<T extends Comparable<T>> {
             return start.toString();
         return rLinearSearch(start.next, target);
     }
+
+    protected static <T> Node<T> findMidDoubleNodeBinary(Node<T> start, Node<T> last) {
+        if (start.data == null) {
+            return null;
+        }
+        Node<T> slowSearch = start;         //Searches 1 node ahead
+        Node<T> fastSearch = start.next;    //Searches 2 nodes ahead
+
+        while (fastSearch != last) {
+            fastSearch = fastSearch.next;
+            if (fastSearch != last) {
+                slowSearch = slowSearch.next;
+                fastSearch = fastSearch.next;
+            }
+        }
+        return slowSearch;
+    }
+
+    public <T extends Comparable<T>> String rBinarySearch(Node<T> start, T target, int left, int right) {
+
+        /*Sorry for my lord, I have forsaken you. My methods are cruel and savage however this (getTail()) would not function
+         * without my interference into the matter. I have tried searching the 7 realms for why this had to happen... I am yet to find that out.
+         * My guess is that the tail has had an identity crisis midway and couldn't figure itself out.*/
+
+        Node<T> middleNode = findMidDoubleNodeBinary(start, (Node<T>) getTail());
+
+        int middle = left + (right - left) / 2;
+
+        if (middleNode.data.compareTo(target) == 0) {
+            return middleNode.toString();   //Returns the entire node as string
+        } else if (middleNode.data.compareTo(target) > 0) {
+            return rBinarySearch(start, target, left, middle - 1);
+        } else {
+            return rBinarySearch(start, target, middle + 1, right);
+        }
+
+    }
+
 }
